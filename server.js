e7bc2ac5b6842df3ea41eee74e34c62cd75ccde5
unknown <kellyd@n-compass.biz>
2025-02-04 15:05:38 +0800

    First Folder Complete

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 //Entry Point
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import posts from './routes/posts.js'
 import logger from './middleware/logger.js';
 import errorHandler from './middleware/error.js';
+import notFound from './middleware/notFound.js';
 const port = process.env.PORT || 8000;
+
+// Get the directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -15,11 +21,15 @@ app.use(express.urlencoded({extended: false}));
 // Logger middleware
 app.use(logger);
 
-// // Setup static folder
-// app.use(express.static(path.join(__dirname, 'public')));
+// Setup static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routers
 app.use('/api/posts', posts);
+
+// Not Found
+app.use(notFound);
+
 
 // Error Handler
 app.use(errorHandler);
